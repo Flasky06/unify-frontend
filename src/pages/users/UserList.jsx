9bc5eddcea71122a838c980c5d88d65a2f6607ab
Bonface Njuguna <bonnienjuguna106@gmail.com>
2025-12-26 01:51:46 +0300

    Remove PageHeader from UserList placeholder

--- src/pages/users/UserList.jsx
+++ src/pages/users/UserList.jsx
@@ -1,9 +1,6 @@
-import { PageHeader } from "../../components/layout/PageHeader";
-
 export const UserList = () => {
   return (
-    <div>
-      <PageHeader title="User Management" subtitle="Manage system users" />
+    <div className="p-6">
       <div className="bg-white p-6 rounded-lg shadow">
         <p>User List Placeholder</p>
       </div>

72602488064074ebeb2a4c976e1d605d0616676d
Bonface Njuguna <bonnienjuguna106@gmail.com>
2025-12-26 23:16:28 +0300

    style: Reduce bottom padding to maximize table height

--- src/components/layout/DashboardLayout.jsx
+++ src/components/layout/DashboardLayout.jsx
@@ -8,7 +8,7 @@ export const DashboardLayout = () => {
       <Sidebar />
       <div className="flex-1 flex flex-col">
         <Header />
-        <main className="flex-1 p-4">
+        <main className="flex-1 px-4 pt-4 pb-1">
           <Outlet />
         </main>
       </div>

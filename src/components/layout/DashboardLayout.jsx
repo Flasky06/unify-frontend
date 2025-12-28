b3e84029c2919f4701970537edbb0865fc2406e5
Bonface Njuguna <bonnienjuguna106@gmail.com>
2025-12-28 18:19:01 +0300

    fix: further reduce mobile padding

--- src/components/layout/DashboardLayout.jsx
+++ src/components/layout/DashboardLayout.jsx
@@ -11,7 +11,7 @@ export const DashboardLayout = () => {
       <Sidebar isOpen={sidebarOpen} onClose={() => setSidebarOpen(false)} />
       <div className="flex-1 flex flex-col overflow-hidden">
         <Header onMenuClick={() => setSidebarOpen(true)} />
-        <main className="flex-1 px-2 sm:px-4 py-4 overflow-auto">
+        <main className="flex-1 px-3 sm:px-4 py-3 sm:py-4 overflow-auto">
           <Outlet />
         </main>
       </div>
